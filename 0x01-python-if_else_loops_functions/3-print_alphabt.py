617119b711fc06ac04383cffd21718c9864faeca
Parsitau-michael <parsitaumichael@gmail.com>
2024-03-14 03:57:27 -0700

    corrected the pycodestyle issue on task 3, yet to correct error in output

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-     if i != ord('e') & i != ord('q'):
-         print('{}'.format(chr(i)), end="")
+    if i != ord('q') & i != ord('e'):
+        print('{}'.format(chr(i)), end="")

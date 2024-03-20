e28cab63c75681ca75b231815500dcc086ca54ec
Parsitau-michael <parsitaumichael@gmail.com>
2024-03-20 12:55:19 -0700

    task 1, a function that finds a value in a list and replaces it

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    func = lambda x : x ** 2
     new_mat = []
     for row in matrix:
-        new_row = list(map(func, row))
+        new_row = list(map(lambda x: x**2, row))
         new_mat.append(new_row)
     return new_mat

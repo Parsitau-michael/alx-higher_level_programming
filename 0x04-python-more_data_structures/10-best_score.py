9b4f6ccbe295830b57a077e603b16f9d5a626c99
Parsitau-michael <parsitaumichael@gmail.com>
2024-03-21 03:08:54 -0700

    task 10, a function to return the max value in a dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    lst = [(key, value) for key, value in a_dictionary.items()]
+    return max(lst)[0]

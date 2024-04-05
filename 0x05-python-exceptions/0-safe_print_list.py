ccb83ef08a46144980b5b185a06bd549867369b4
Parsitau-michael <parsitaumichael@gmail.com>
2024-04-05 13:20:14 -0700

    Just concluded one of my lines with a Semi colon, Can you Imagine that

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,7 +11,7 @@ def safe_print_list(my_list=[], x=0):
     Return: the actual number printed.
     """
     try:
-        count = 0;
+        count = 0
         for i in range(x):
             print(my_list[i], end="")
             count += 1

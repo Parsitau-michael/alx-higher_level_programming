063dc2a7ea4dd9e051e0477b811f2f6ddbd63307
Parsitau-michael <parsitaumichael@gmail.com>
2024-04-05 13:18:04 -0700

    a function that safely prints items of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+this module is of a function that safely prits the values of a list
+"""
+
+
+def safe_print_list(my_list=[], x=0):
+    """
+    my_list - is a list that may contain any type.
+    x - represents the number of  elements to print
+    Return: the actual number printed.
+    """
+    try:
+        count = 0;
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+        print()
+        return count
+    except IndexError:
+        print()
+        return count

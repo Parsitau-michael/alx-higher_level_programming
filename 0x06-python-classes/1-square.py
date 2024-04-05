418e4a74c38928baecf52919682b77fd3dd63682
Parsitau-michael <parsitaumichael@gmail.com>
2024-04-05 11:17:27 -0700

    Initialized a private instance attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines a Square Class
+"""
+
+
+class Square:
+    """
+    This class represents a Square.
+    Size is a private instance attribute.
+    """
+    def __init__(self, size):
+        self.__size = size

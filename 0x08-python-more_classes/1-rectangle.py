116fe5c3cf93d1772d6d941676d6ef8dd546b331
Parsitau-michael <parsitaumichael@gmail.com>
2024-04-08 13:56:28 -0700

    Had initialized __height as width

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
     """
     def __init__(self, width=0, height=0):
         self.__width = width
-        self.__height = width
+        self.__height = height
 
     @property
     def width(self):
@@ -36,4 +36,4 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         else:
-            self.__height
+            self.__height = value

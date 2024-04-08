bd0ceedad5c74a0c5e32db4e20995a9ca1bd052e
Parsitau-michael <parsitaumichael@gmail.com>
2024-04-08 12:44:34 -0700

    Created an empty class object

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+this module defines an empty class Rectangle
+"""
+
+class Rectangle:
+    """
+    This represents a class object
+    Currently it does nothing
+    """
+    pass

feebf90e34c2741008f97bf7ccd96923a1ef8818
Parsitau-michael <parsitaumichael@gmail.com>
2024-04-15 23:26:25 -0700

    I had a line with words > 79, pycodestyle wasnt happy

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,8 +3,10 @@
 
 
 def lookup(obj):
-    """This function returns a list of available attributes and methods of an object.
+    """This function returns a list of available attributes
+    and methods of an object.
 
-    It takes an object as an argument, returns a list of attributes and methods of the object.
+    It takes an object as an argument,
+    returns a list of attributes and methods of the object.
     """
     return dir(obj)

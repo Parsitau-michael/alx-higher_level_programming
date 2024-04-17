b6a3e5320381b9054dd3ab463daa5d67b54f90fa
Parsitau-michael <parsitaumichael@gmail.com>
2024-04-17 11:20:48 -0700

    Removed the nextline character at the end of the file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-    """this function opens a file and reads its content"""
-    with open(filename, mode='r', encoding="utf-8") as f:
-        print(f.read())
+    """ A function to read a textfile and print its content to stdout """
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read(), end="")

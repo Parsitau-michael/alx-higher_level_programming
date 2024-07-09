b7ac1b026be3d43484be4911aa938d660f8d439d
Parsitau-michael <parsitaumichael@gmail.com>
2024-07-09 09:39:42 -0700

    task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -s -X -I OPTIONS "$1" | grep "Allow:" | tr -d '\r'
+curl -s -I "$1" | grep "Allow" | cut -d " " -f 2

822d799f964d466a6cccda6878f49cbdd42bbdda
Parsitau-michael <parsitaumichael@gmail.com>
2024-07-10 04:19:34 -0700

    Corrected module documentation errors

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
 
-from sys import argv
-import MySQLdb
-
 """
 This module demonstrates using Python with MySQL
 """
 
-
-""" Establishing connection to the database """
-db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-                     db=argv[3], port=3306)
-cur = db.cursor()
-
-""" Executing the Query """
-search = argv[4]
-cur.execute("SELECT * FROM states WHERE states.name = '{}' ORDER BY states.id".
-            format(search))
-
-""" Fetch all rows """
-rows = cur.fetchall()
-
-""" Printing each row """
-for row in rows:
-    print("({}, '{}')".format(row[0], row[1]))
-
-""" Close the Cursor and the DB """
-cur.close()
-db.close()
+from sys import argv
+import MySQLdb
 
 if __name__ == "__main__":
-    pass
+    """ Establishing connection to the database """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+                         db=argv[3], port=3306)
+    cur = db.cursor()
+
+    """ Executing the Query """
+    search = argv[4]
+    query = """
+        SELECT *
+        FROM states
+        WHERE states.name = '{}'
+        ORDER BY states.id
+        """
+    cur.execute(query.format(search))
+
+    """ Fetch all rows """
+    rows = cur.fetchall()
+
+    """ Printing each row """
+    for row in rows:
+        print("({}, '{}')".format(row[0], row[1]))
+
+        """ Close the Cursor and the DB """
+        cur.close()
+        db.close()

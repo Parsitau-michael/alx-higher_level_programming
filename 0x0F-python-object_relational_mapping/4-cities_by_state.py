0bc6ee74f7d477f097e571f0f02fe5e41751ab7c
Parsitau-michael <parsitaumichael@gmail.com>
2024-07-10 04:38:28 -0700

    Correctly placed my module documentation

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,38 +1,37 @@
 #!/usr/bin/python3
 
-from sys import argv
-import MySQLdb
-
 """
 This module demonstrates using Python with MySQL
 """
 
 
-""" Establishing connection to the database """
-db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-                     db=argv[3], port=3306)
-cur = db.cursor()
-
-""" Executing the Query """
-query = """
-    SELECT cities.id AS city_id, cities.name AS city_name,
-    states.name AS state_name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    ORDER BY cities.id
-    """
-cur.execute(query)
-
-""" Fetch all rows """
-rows = cur.fetchall()
-
-""" Printing each row """
-for row in rows:
-    print("({}, '{}', '{}')".format(row[0], row[1], row[2]))
-
-""" Close the Cursor and the DB """
-cur.close()
-db.close()
+from sys import argv
+import MySQLdb
 
 if __name__ == "__main__":
-    pass
+    """ Establishing connection to the database """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+                         db=argv[3], port=3306)
+    cur = db.cursor()
+
+    """ Executing the Query """
+    query = """
+        SELECT cities.id AS city_id, cities.name AS city_name,
+        states.name AS state_name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id
+        """
+    cur.execute(query)
+
+    """ Fetch all rows """
+    rows = cur.fetchall()
+
+    """ Printing each row """
+    for row in rows:
+        print("({}, '{}', '{}')".format(row[0], row[1], row[2]))
+
+    """ Close the Cursor and the DB """
+    cur.close()
+    db.close()

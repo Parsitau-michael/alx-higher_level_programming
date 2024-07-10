deffff038cae0ca229cf5fb12229dd07acde47cc
Parsitau-michael <parsitaumichael@gmail.com>
2024-07-10 04:04:50 -0700

    Corrected a few pycodestyle and documentation errors

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,32 +1,39 @@
 #!/usr/bin/python3
 
-from sys import argv
-import MySQLdb
-
 """
 This module demonstrates using Python with MySQL
 """
 
 
-""" Establishing connection to the database """
-db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-                     db=argv[3], port=3306)
-cur = db.cursor()
-
-""" Executing the Query """
-cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%'
-            ORDER BY states.id")
-
-""" Fetch all rows """
-rows = cur.fetchall()
-
-""" Printing each row """
-for row in rows:
-    print("({}, '{}')".format(row[0], row[1]))
-
-""" Close the Cursor and the DB """
-cur.close()
-db.close()
+from sys import argv
+import MySQLdb
 
 if __name__ == "__main__":
-    pass
+    """ Establishing connection to the database """
+
+    db = MySQLdb.connect(
+                         host="localhost",
+                         user=argv[1], passwd=argv[2],
+                         db=argv[3],
+                         port=3306)
+    cur = db.cursor()
+
+    """ Executing the Query """
+    query = """
+        SELECT *
+        FROM states
+        WHERE states.name LIKE 'N%'
+        ORDER BY states.id
+        """
+    cur.execute(query)
+
+    """ Fetch all rows """
+    rows = cur.fetchall()
+
+    """ Printing each row """
+    for row in rows:
+        print("({}, '{}')".format(row[0], row[1]))
+
+    """ Close the Cursor and the DB """
+    cur.close()
+    db.close()

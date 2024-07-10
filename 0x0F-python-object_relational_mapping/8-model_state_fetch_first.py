4e5129d1a594eff6b18d777125612d853b07521a
Parsitau-michael <parsitaumichael@gmail.com>
2024-07-10 05:03:01 -0700

    Print Nothing if table is empty

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -26,6 +26,6 @@ if __name__ == "__main__":
     if first_state:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
-        print("")
+        print("Nothing")
 
     session.close()

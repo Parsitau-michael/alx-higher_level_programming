e2ab8cdba7f915835c86764d31b21d6f2004a744
Parsitau-michael <parsitaumichael@gmail.com>
2024-08-01 03:34:22 -0700

    Task 3 correction

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request({ url: url, json: true }, function (error, response, body) {
   if (error) {

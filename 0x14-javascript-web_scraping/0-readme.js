8ad172fda83831901d61b71b8236bde79c2f2c2f
Parsitau-michael <parsitaumichael@gmail.com>
2024-07-31 20:05:52 -0700

    Corrected a few semistandard errors

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
-let fs = require('fs');
+const fs = require('fs');
 
-let read_string = fs.readFile(process.argv[2], 'utf8', function (err, data) {
-	if (err)
-		return console.error(err);
-	console.log(data);
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(data);
 });
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
-let fs = require('fs');
+const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', function(err, data) {
-	if (err)
-		console.error(err);
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err, data) {
+  if (err) {
+    console.error(err);
+  }
 });
